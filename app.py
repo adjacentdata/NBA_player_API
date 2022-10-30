68c8964fe455c62163c182a11cb0eb1267622535
Albert Perez <ayybeef@Alberts-MacBook-Pro.local>
2022-10-30 16:29:30 -0700

    moved database for testing to app

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask 
-from database import db1
+from dotenv import load_dotenv
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app= Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRE_PATH")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Players(db.Model):
+    player_id = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(80), nullable=False)
+    team_id = db.Column(db.Integer, nullable=False)
+    season = db.Column(db.Date)
 
 @app.route('/')
 def hello():
