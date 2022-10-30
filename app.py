26be34988496288e64c7496d588a1b05481eff5c
Albert Perez <ayybeef@Alberts-MacBook-Pro.local>
2022-10-30 16:32:41 -0700

    Added documentation

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from dotenv import load_dotenv
 import os
 from flask_sqlalchemy import SQLAlchemy
 
+#Load environment variables
 load_dotenv()
 
+# Configure PostgreSQL to SQL Alchemy with environment variable
 app= Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRE_PATH")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Create the database object - (To move later)
 db = SQLAlchemy(app)
 
 class Players(db.Model):
@@ -19,4 +22,7 @@ class Players(db.Model):
 
 @app.route('/')
 def hello():
+    """
+     Returns the application test hello 
+    """
     return "Hello"
