ef5d7b21b5af98c73d894a72da735759e413f402
Albert Perez <ayybeef@Alberts-MacBook-Pro.local>
2022-10-29 20:08:47 -0700

    added configuration data

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "config"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+
 db1 = SQLAlchemy(app)
 
