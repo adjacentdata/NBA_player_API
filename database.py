e1e7876df59759bd0d1e0dbbee9a068724354294
Albert Perez <ayybeef@Alberts-MacBook-Pro.local>
2022-10-29 20:20:53 -0700

    Built Data Model

--- database.py
+++ database.py
@@ -8,3 +8,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db1 = SQLAlchemy(app)
 
+class Players(db1.Model):
+    player_id = db1.Column(db1.Integer, primary_key=True)
+    player_name = db1.Column(db1.String(80), nullable=False)
+    team_id = db1.Column(db1.Integer, nullable=False)
+    season = db1.Column(db1.Date)
