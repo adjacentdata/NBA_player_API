9b316e5efd0a3b06f96b3b2fb6a715d819bd4daa
AJ Perez <atpcodeio@gmail.com>
2022-10-31 21:22:47 -0700

    Created Player template

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from dataclasses import field
 from marshmallow import Schema, fields
 
-class player_schema(Schema):
+class Player_Schema(Schema):
     player_id = fields.Integer()
     team_id = fields.Integer()
     name = fields.String()
     season = fields.Date()
-    
